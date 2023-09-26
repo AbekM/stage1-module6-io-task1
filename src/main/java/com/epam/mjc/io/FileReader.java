488fd8b00c6339d89772d0934dd1836f28f0c9f5
AbekM <asilbek.makhmudov@outlook.com>
2023-09-26 22:50:42 +0500

    smth

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -2,31 +2,40 @@ package com.epam.mjc.io;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 
 
 public class FileReader {
 
-    String line = null;
-    Map<String, String> result = new HashMap<>();
-    String[] array = {};
     public Profile getDataFromFile(File file) {
-
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file))))
-        {
-            while ((line = reader.readLine()) != null) {
-                array = line.split(":");
-                result.put(array[0], array[1]);
+        String name = null;
+        Integer age = null;
+        Long phone = null;
+        String email = null;
+        String tmp;
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br;
+        try (java.io.FileReader fr = new java.io.FileReader(file)) {
+            br = new BufferedReader(fr);
+            while ((tmp = br.readLine()) != null) {
+                sb.append(tmp).append(";");
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        return new Profile(result.get("Name").trim(), Integer.parseInt(result.get("Age").trim()), result.get("Email").trim(), Long.parseLong(result.get("Phone").trim()));
+        tmp = sb.toString();
+        String[] response = tmp.split(";");
+        for (String s : response) {
+            if (s.startsWith("Name")) {
+                name = s.substring(6);
+            } else if (s.startsWith("Age")) {
+                age = Integer.parseInt(s.substring(5));
+            } else if (s.startsWith("Email")) {
+                email = s.substring(7);
+            } else if (s.startsWith("Phone")) {
+                phone = Long.parseLong(s.substring(7));
+            }
+        }
+        return new Profile(name, age, email, phone);
     }
 }
