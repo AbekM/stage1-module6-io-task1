aac9d9470bc53ced36380d6cdadf720d7ad523b7
AbekM <asilbek.makhmudov@outlook.com>
2023-09-26 22:33:45 +0500

    smth

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -19,25 +19,22 @@ public class FileReader {
         try (java.io.FileReader fr = new java.io.FileReader(file)) {
             br = new BufferedReader(fr);
             while ((tmp = br.readLine()) != null) {
-                sb.append(tmp + ";");
+                sb.append(tmp).append(";");
             }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
         tmp = sb.toString();
         String[] response = tmp.split(";");
-        for (int i = 0; i < response.length; i++){
-            System.out.println(response[i]);
-            if (response[i].startsWith("Name")) {
-                name = response[i].substring(6);
-            } else if (response[i].startsWith("Age")) {
-                age = Integer.parseInt(response[i].substring(5));
-            } else if (response[i].startsWith("Email")) {
-                email = response[i].substring(7);
-            } else if (response[i].startsWith("Phone")) {
-                phone = Long.parseLong(response[i].substring(7));
+        for (String s : response) {
+            if (s.startsWith("Name")) {
+                name = s.substring(6);
+            } else if (s.startsWith("Age")) {
+                age = Integer.parseInt(s.substring(5));
+            } else if (s.startsWith("Email")) {
+                email = s.substring(7);
+            } else if (s.startsWith("Phone")) {
+                phone = Long.parseLong(s.substring(7));
             }
         }
         return new Profile(name, age, email, phone);
